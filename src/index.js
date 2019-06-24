92c935bb338f21ee79e0865f74c20256fc0d875c
your name <ajuonuolivia@gmail.com>
2019-06-24 13:22:19 +0100

    this is my third commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import  Likes from './component/Likes';
 import Todo from './component/Todo';
 import * as serviceWorker from './serviceWorker';
 
 
-ReactDOM.render(<Todo/>, document.getElementById('root'));
+ReactDOM.render(<Likes/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
