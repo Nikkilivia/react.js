e6352625441fafa9f95c30e46f76bcc492969ab7
your name <ajuonuolivia@gmail.com>
2019-06-24 14:50:32 +0100

    my fourth commit

--- src/component/Todo.js
+++ src/component/Todo.js
@@ -3,17 +3,36 @@ import TodoItems from './TodoItems';
 import Header from './Header'
 
 class Todo extends Component{
+    state={
+        todoItems:['study','sleep','eat'],
+        newTodo:''
+
+    };
+    handleChange=e =>{
+        this.setState({newTodo:e.target.value})
+        console.log(e.target.value,"valueeeeee");
+    };
+    handleSubmit = (e)=>{
+        e. preventDefault()
+        console.log(this.state.newTodo)
+    }
     render(){
-        const todoItems = ['study','sleep','eat']
         return(
             <div>
                 <Header title="MY TODO TITLE"/>
                 <h1>welcome to my todo App</h1>
-                <ul>
+                
                     {
-                        todoItems.map(item=>(<TodoItems individualItem={item}/>))
+                        this.state.todoItems.map(item=>(<TodoItems individualItem={item}/>))
                     }
-                </ul>
+                <form onSubmit={this.handleSubmit}>
+           <label htmlFor="">Todo Items</label><br />
+           <input type =
+           "text" name="new-Todo-item"
+            value={this.state.newtodo}
+            onChange={this.handleChange}/><br />
+           <button>Submit</button>
+                </form>
                 
             </div>
            
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Todo from './component/Todo';
 import * as serviceWorker from './serviceWorker';
 
 
-ReactDOM.render(<Likes/>, document.getElementById('root'));
+ReactDOM.render(<Todo/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
